ea96ac06f2252ec293d10db77a00833383680ba4
Saidi Oussama <saidi-ouss@outlook.com>
2024-08-04 15:07:31 +0200

    Add manifest file

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Web Tank View',
+    'version': '17.0',
+    'category': 'Warehouse',
+    'summary': 'Add a tank view for stock locations',
+    'description': """
+        This module adds a custom view for stock locations to display them in a tank format.
+    """,
+    'author': 'Saidi Oussama',
+    'depends': ['stock', 'web'],
+    'data': [
+       
+    ],
+    'qweb': [
+        
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
